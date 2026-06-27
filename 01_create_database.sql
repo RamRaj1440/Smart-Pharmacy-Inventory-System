9d2b47db82610d3083e89f256cd3fb2d8f4c4922
RamRaj <102046374+RamRaj1440@users.noreply.github.com>
2026-06-27 17:59:47 +0530

    Create Add database creation scriptdatabase for Smart Pharmacy Inventory system
    This script creates the database for the Smart Pharmacy Inventory & Expiry Management System.

---
+++ 01_create_database.sql
@@ -0,0 +1,11 @@
+-- =====================================================
+-- Smart Pharmacy Inventory & Expiry Management System
+-- File: 01_create_database.sql
+-- Description: Create the project database
+-- =====================================================
+
+DROP DATABASE IF EXISTS smart_pharmacy_inventory;
+
+CREATE DATABASE smart_pharmacy_inventory;
+
+USE smart_pharmacy_inventory;
