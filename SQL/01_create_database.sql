5cf2499119d906c7496ff3f5bda79ae0aa408d73
RamRaj <102046374+RamRaj1440@users.noreply.github.com>
2026-06-29 10:18:18 +0530

    Refactor database creation script
    Removed redundant database creation and added SHOW DATABASES command.

--- SQL/01_create_database.sql
+++ SQL/01_create_database.sql
@@ -3,9 +3,9 @@
 -- File: 01_create_database.sql
 -- Description: Create the project database
 -- =====================================================
-
-DROP DATABASE IF EXISTS smart_pharmacy_inventory;
-
-CREATE DATABASE smart_pharmacy_inventory;
+CREATE DATABASE  smart_pharmacy_inventory;
 
 USE smart_pharmacy_inventory;
+
+SHOW DATABASES;
+
